2cfc1e3f7b143761b0dc600b3e3b68948f421f56
soenneker <jake@soenneker.com>
2026-08-29 14:17:26 -0500

    Improve public API XML documentation

--- src/Soenneker.Validators.IpAddresses.Ssrf/Registrars/SsrfIpAddressValidatorRegistrar.cs
+++ src/Soenneker.Validators.IpAddresses.Ssrf/Registrars/SsrfIpAddressValidatorRegistrar.cs
@@ -12,6 +12,8 @@ public static class SsrfIpAddressValidatorRegistrar
     /// <summary>
     /// Adds <see cref="ISsrfIpAddressValidator"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddSsrfIpAddressValidatorAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<ISsrfIpAddressValidator, SsrfIpAddressValidator>();
@@ -22,6 +24,8 @@ public static class SsrfIpAddressValidatorRegistrar
     /// <summary>
     /// Adds <see cref="ISsrfIpAddressValidator"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddSsrfIpAddressValidatorAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<ISsrfIpAddressValidator, SsrfIpAddressValidator>();
